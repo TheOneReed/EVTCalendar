deb3d6fc10424a8cdc35a1a085cd4cb5c51cee6b
Thomas Hansen <Dryvnt@gmail.com>
2016-11-17 19:04:44 +0100

    cleaning up global variables, functions

--- EVTCompute.lua
+++ EVTCompute.lua
@@ -1,11 +1,6 @@
-table_Months = {EVT_JAN, EVT_FEB, EVT_MAR, EVT_APR, EVT_MAY, EVT_JUN, EVT_JUL, EVT_AUG, EVT_SEP, EVT_OCT, EVT_NOV, EVT_DEC };
+table_Months = {EVT_JAN, EVT_FEB, EVT_MAR, EVT_APR, EVT_MAY, EVT_JUN, EVT_JUL, EVT_AUG, EVT_SEP, EVT_OCT, EVT_NOV, EVT_DEC};
 
-table_Dotw = { EVT_SUN, EVT_MON, EVT_TUE, EVT_WED, EVT_THU, EVT_FRI, EVT_SAT };
-
-month = nil;
-day = nil;
-year = nil;
-dotw = nil;
+table_Dotw = {EVT_SUN, EVT_MON, EVT_TUE, EVT_WED, EVT_THU, EVT_FRI, EVT_SAT};
 
 local epochYear = 2016;
 local epochMonth = 1; --whichever month your epoch is
@@ -14,32 +9,16 @@ local epochDay = 5; --whichever day your epoch is
 function isLeapYear(year)
     local naiveLeap = (mod(year, 4) == 0);
     local centuryClause = (mod(year, 100) ~= 0);
-    local centuryException = (mod(year, 400) == 0);
-    local smartLeap = naiveLeap and (centuryClause or centuryException);
-    return smartLeap;
+    local centuryClauseException = (mod(year, 400) == 0);
+    return naiveLeap and (centuryClause or centuryException);
 end
 
 function DaysInMonth(year, month)
-    local daysPerMonth = {
-        [1] = 31;
-        [2] = 28;
-        [3] = 31;
-        [4] = 30;
-        [5] = 31;
-        [6] = 30;
-        [7] = 31;
-        [8] = 31;
-        [9] = 30;
-        [10] = 31;
-        [11] = 30;
-        [12] = 31;
-        ["leap"] = 29;
-    };
-    
-    local isFeb = (month == 2);
-    if (isFeb and isLeapYear(year)) then
-        return daysPerMonth["leap"]
+    if month == 2 and isLeapYear(year) then
+        return 29;
     end
+    
+    local daysPerMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     return daysPerMonth[month]
 end
 
